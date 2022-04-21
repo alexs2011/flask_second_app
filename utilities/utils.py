a6425c04e915e53cdbb6399fce760288c01c131c
Alexander <kir-al93@yandex.ru>
2022-04-21 11:28:34 +0900

    Added load_data func

---
+++ utilities/utils.py
@@ -0,0 +1,9 @@
+import json
+
+
+def load_data(filename: str) -> list[dict]:
+    """
+    Загружает данные из файла формата JSON.
+    """
+    with open(filename, 'r', encoding='utf-8') as f_in:
+        return json.load(f_in)
